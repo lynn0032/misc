a9420d10531a98c38f1e994e99eddf6f32d81e64
lynn0032 <33353757+lynn0032@users.noreply.github.com>
2021-07-31 18:34:44 -0500

    Add files via upload

---
+++ FarmersMarketOuterWorkflow.py
@@ -0,0 +1,16 @@
+# @begin farmers_market_cleaning
+# @in farmers_markets.csv
+# @out farmers_market_final.csv
+
+# @begin FarmersMarketFinalClean.ipynb
+# @in farmers_markets.csv
+# @out farmers_market_all_cols.csv
+# @out farmers_market_final.csv
+# @out farmers_market_gps_repaired.csv
+# @end Jupyter Notebook: FarmersMarketFinalClean.ipynb
+
+# @begin FarmersMarketFinalIntegrityCheck.ipynb
+# @in farmers_market_all_cols.csv
+# @end Jupyter Notebook: FarmersMarketFinalIntegrityCheck.ipynb
+
+# @end farmers_market_cleaning
